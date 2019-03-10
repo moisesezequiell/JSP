431c0031237648dea2d5af0665dec6dfefd7eb9c
moisesezequiell <moisesktrina>
2019-03-09 22:57:11 -0300

    commit

---
+++ src/br/com/fastshop/model/Cliente.java
@@ -0,0 +1,50 @@
+package br.com.fastshop.model;
+
+public class Cliente {
+	
+	private int id;
+	private String nome;
+	private String documento;
+	private int idade;
+
+	public Cliente() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + ", documento=" + documento + ", idade=" + idade + "]";
+	}
+	
+}
